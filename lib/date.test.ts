d83e1dca7daf25399eed025169ea426bb851ebd6
Otto Martikainen <martikainenotto@gmail.com>
2025-09-28 14:42:44 +0300

    Add unit tests with Vitest

---
+++ lib/date.test.ts
@@ -0,0 +1,19 @@
+import { expect, test } from 'vitest'
+import { addDays, nextMonth, startOfMonth } from './date'
+
+test('addDays', () => {
+  expect(addDays(new Date(2000, 1, 1), 1)).toEqual(new Date(2000, 1, 2))
+  expect(addDays(new Date(2000, 1, 1), -1)).toEqual(new Date(1999, 12, 31))
+  expect(addDays(new Date(2000, 2, 28), 1)).toEqual(new Date(2000, 2, 29))
+})
+
+test('startOfMonth', () => {
+  expect(startOfMonth(new Date(2000, 1, 1))).toEqual(new Date(2000, 1, 1))
+  expect(startOfMonth(new Date(2000, 2, 29))).toEqual(new Date(2000, 2, 1))
+})
+
+test('nextMonth', () => {
+  expect(nextMonth(new Date(2000, 1, 1))).toEqual(new Date(2000, 2, 1))
+  expect(nextMonth(new Date(2000, 2, 29))).toEqual(new Date(2000, 3, 1))
+  expect(nextMonth(new Date(2000, 12, 15))).toEqual(new Date(2001, 1, 1))
+})
